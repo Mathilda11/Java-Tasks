651579d1722ec9444599d9b14733328c35c0f73f
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2019-03-22 15:47:33 +0800

    Update Utils.java

--- doubanmovies/doubanmovies/src/main/java/com/douban/Utils.java
+++ doubanmovies/doubanmovies/src/main/java/com/douban/Utils.java
@@ -10,11 +10,7 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-/**
- * 站点日志数据工具类
- * @author Administrator
- *
- */
+
 public class Utils {
 	
 	/**
